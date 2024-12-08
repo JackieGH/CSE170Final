27ee2805af2bd925a9e4b188d26920902f91fe56
Jackie Maldonado <118325713+JackieGH@users.noreply.github.com>
2024-12-08 12:05:19 -0800

    updates to code

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
+// interaction 1 //
+const button1 = document.getElementById("interactbutton1");
+const sections = document.querySelectorAll(".section");
 
+const targetSection = document.querySelector(".section:nth-child(3)");
+
+button1.addEventListener("click", () => {
+    button1.style.backgroundColor = "pink";
+    button1.style.color = "white";
+
+
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+
+    targetSection.style.display = "block";
+});
+
+// interaction 2 //
+const likeButtons = document.querySelectorAll(".like-btn");
+
+const fullHeart = 'https://www.svgrepo.com/show/525369/heart.svg' 
+
+likeButtons.forEach(image => {
+    image.addEventListener("click", () => {
+        image.src = fullHeart;
+    });
+});
   
